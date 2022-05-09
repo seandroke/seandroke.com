e7f6a2edb708f88c6ff19e0bb3ddbf1aa6acaf79
Sean Droke <SeanDroke@MacbookPros-MacBook-Pro.local>
2022-05-09 12:17:01 -0400

    Docker refactoring.

--- prod.sh
+++ prod.sh
@@ -4,7 +4,7 @@ docker build -f backend/Dockerfile -t seandrokecom_backend backend/.
 docker build -f frontend/Dockerfile -t seandrokecom_frontend frontend/.
 docker build -f server/Dockerfile -t seandrokecom_server server/.
 
-docker run -d -p 27017:27017 -v db-data:/data/db --network=seandrokecom --name=db seandrokecom_database
-docker run -d --network=seandrokecom --name=backend seandrokecom_backend
-docker run -d --network=seandrokecom --name=frontend seandrokecom_frontend
-docker run -d -p 8080:80 --network=seandrokecom --name=server seandrokecom_server
+docker run -d -p 27017:27017 -v db-data:/data/db --restart=always --network=seandrokecom --name=db seandrokecom_database
+docker run -d --restart=always --network=seandrokecom --name=backend seandrokecom_backend
+docker run -d --restart=always --network=seandrokecom --name=frontend seandrokecom_frontend
+docker run -d -p 8080:80 --restart=always --network=seandrokecom --name=server seandrokecom_server
