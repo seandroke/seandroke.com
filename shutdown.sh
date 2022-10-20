43b13f9f3e5ad14e81c28eefd094b3a2dbaac522
Sean Droke <SeanDroke@MacBook-Pro.local>
2022-10-20 15:18:17 -0400

    Updates October 2022

--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,8 @@
-docker stop seandrokecom_server
-docker rm seandrokecom_server
-docker stop seandrokecom_backend
-docker rm seandrokecom_backend
-docker stop seandrokecom_database
-docker rm seandrokecom_database
-docker stop seandrokecom_frontend
-docker rm seandrokecom_frontend
+docker stop server
+docker rm server
+docker stop backend
+docker rm backend
+docker stop db
+docker rm db
+docker stop frontend
+docker rm frontend
