303d1cefd4736c19d5955c6ca502fb694e68f791
Sean Droke <SeanDroke@MacBook-Pro.local>
2023-03-15 18:25:48 -0400

    Updates March 2023

--- prod.sh
+++ prod.sh
@@ -1,8 +1,8 @@
 docker network create seandrokecom
-docker build -f database/Dockerfile -t seandrokecom_database database/.
-docker build -f backend/Dockerfile -t seandrokecom_backend backend/.
-docker build -f frontend/Dockerfile -t seandrokecom_frontend frontend/.
-docker build -f server/Dockerfile -t seandrokecom_server server/.
+docker build --no-cache -f database/Dockerfile -t seandrokecom_database database/.
+docker build --no-cache -f backend/Dockerfile -t seandrokecom_backend backend/.
+docker build --no-cache -f frontend/Dockerfile -t seandrokecom_frontend frontend/.
+docker build --no-cache -f server/Dockerfile -t seandrokecom_server server/.
 
 docker run -d -p 27017:27017 -v db-data:/data/db --restart=always --network=seandrokecom --name=db seandrokecom_database
 docker run -d --restart=always --network=seandrokecom --name=backend seandrokecom_backend
